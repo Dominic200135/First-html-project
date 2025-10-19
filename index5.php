f591323e17a7d612493368dc303ba70ef26f769d
Dominic200135 <brabidominic1@gmail.com>
2025-10-19 20:05:34 +0000

    Add files via upload

---
+++ index5.php
@@ -0,0 +1,318 @@
+
+<!DOCTYPE html>
+<html><head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/index5.css">
+	<link rel="stylesheet" type="text/css" href="css/project-media.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer">
+	<title>OC DERMATOLOGY</title>
+</head>
+<body>
+	<div id="header">
+	    <div class="top-line"></div>
+		<div id="offset">
+		    <div class="logo-bar">
+		    	<a href="/website/index4.php">
+			    	<div class="logo">
+			    	    <img src="images/logo.jpeg">
+			    	</div>
+			    </a>
+		    	<div class="contact-info">
+		    		<div class="name">Dermatology &amp; Laser Center</div>
+		    		<div class="contact">Call <b style="font: bold;"><a href="tel:+23352332356">(949) 363-1788</a></b> Orange County, CA</div>
+		    	</div>
+		    	<div class="other-links">
+		    		<div class="o-link">
+		    		    <div class="p-login">
+		    			    <a href="website"><span style="color:#3399cc; text-size:small;">PATIENT LOG-IN </span></a>
+		    		    </div>
+		    		    <div class="vertical" style="color: #3399cc;"></div>
+		    		    <div class="p-resource">
+		    			    <a href=""><span style="color:#3399cc; text-size:small:">PATIENT RESOURCES</span></a>
+		    		    </div>
+		    		</div>
+		    		<div class="social-icons">
+		    		    <i class="fa-brands fa-square-facebook"></i>
+		    		    <i class="fa-brands fa-square-twitter"></i>
+		    		    <i class="fa-brands fa-square-youtube"></i>
+		    		    <i class="fa-brands fa-blogger"></i>
+		    		</div>
+		    	</div>
+		    </div>   	
+		    <div class="nav">
+		    	<a href="#"><span>GENERAL &amp; SURGICAL DERMATOLOGY</span>
+<span class="fa fa-caret-down"></span></a>
+		    	<a href="#"><span>COSMETIC DERMATOLOGY</span>
+    <span class="fa fa-caret-down"></span></a>
+		    	<a href="#"><span>PHOTO GALLERY</span></a>
+		    	<a href="#"><span>NEWS &amp; SPECIALS</span></a>
+		    	<a href="#"><span>ABOUT</span></a>
+		    	<a href="#"><span>TESTIMONIALS</span></a>
+		    	<a href="#"><span>CONTACT</span></a>
+		    </div>	
+		    <div class="diseases">
+		    	<div class="acne">
+		    	    <a href="#"><span>Acne Treatment</span></a>
+		    	</div>
+		    	<div class="vertical-line"></div>
+		    	<div class="eczema">
+		    	    <a href="#"><span>Eczema Treatment</span></a>		
+		    	</div>
+		    	<div class="vertical-line"></div>
+		    	<div class="skin-c">
+		    	    <a href="#"><span>Skin Cancer Diagnosis &amp; Treatment</span></a>
+		    	</div>
+		    </div>
+		</div>	
+	</div>
+	<div id="featured_image" class="ftd_image bgt_set">
+	    <div class="text-image_1">
+		    <div class="big-container">
+		        <div class="text1">In our hands, <br><b style="font:bold;background: #326efa;color: #fff;padding: 3px 10px;">your face</b> will look its best.<br></div>
+		        <div class="image_text" style="position: relative;">
+    				<div id="overlay" style="position: absolute;top: 0;bottom: 0;left: 0;right: 0;background-image: linear-gradient(270deg,#fff 0%, #fff 55%,transparent);"></div>
+			        <div id="s-img" class="s_image bgt_set"></div>
+			        <div class="s-text"><b style="color:#6a4c93;">Dr. Lorrie J. Klein</b> and OC Dermatology are among the <br>Top 10 dermatologists in the United States in<br>BOTOX® &amp; JUVEDERM® treatments.</div>
+ 				</div>	
+		    </div>
+	    </div>
+	    <div class="text-image_2">
+		    <div id="small-container">
+		        <div id="small_image" class="small-image bgt_set"></div>
+		        <div class="text2"><b style="color:#fff; font: bold;">Join to</b> receive our free E-Beauty Lifestyle<br>magazine and <b style="font:bold; color:#fff;">receive special offers</b><br></div>
+		    </div>
+		    <div class="signup">
+		    	<a href="#"><span>SIGNUP NOW!</span></a>
+		    </div>
+	    </div>
+	</div>
+	<div id="section">
+        <div id="section_1" style="text-align: center;">
+            <h1>Welcome to OC Dermatology</h1>
+            <p style="color:#7c7777;">We take pride in our ability to provide comprehensive cosmetic and medical dermatology treatments</p>    
+        </div>
+        <div id="section_2">
+        	<div id="image" class="image bgt_set"></div>
+        	<div class="text3">
+        		<div class="col_1"><b style="font:bold;">Dr. Lorrie Klein</b></div>
+        		<div class="col_2">1. Dr. Lorrie Klein and OC Dermatology are among the <b style="color: #3a86ff;;">Top 10 dermatologists</b> in the United States in BOTOX® &amp; JUVEDERM®
+                treatments.</div>
+        		<div class="col_3">2. Dr. Klein is the <b style="color: #3a86ff;;">#1 dermatologist</b> in Orange County and San Diego County in BOTOX® &amp; JUVEDERM® treatments among all dermatologists and plastic surgeons.</div>
+        		<div class="col_4">3. And Dr. Klein is among the <b style="color: #3a86ff;;">Top 100</b> of all physicians in the world using these treatments.</div>
+        	</div>
+        </div>
+        <div class="section_3">
+        	<div class="video-1">
+        		<video src="./videos/video 1.mp4" height="200px" controls="" autoplay="" muted=""></video>
+        	</div>
+        	<div class="video-2">
+        		<video src="./videos/video 2.mp4" height="200px" controls="" autoplay="" muted=""></video>
+        	</div>
+        </div>
+        <div class="section_4">
+        	<div class="col-1">
+        		<div id="col-text-1" class="col-text " style="position: absolute;z-index: 1;background-image: linear-gradient(45deg, #d4dfe1, transparent);width: 100%;">    			<div class="f-text1"><b style="font:bold;">NEW PATIENT<br>SPECIALS<br></b></div>
+        			<div class="f-text2">Enjoy specials discount<br>on your first visit!<br></div>
+        			<div class="f-text3">
+        			    <a href="#"><span>Learn more</span></a>
+        			</div>
+        		</div>
+        		<div id="col-img-1" class="col-img" style="right: 0;"></div>
+        	</div>
+        	<div class="col-2">
+                <div id="col-text-2" class="col-text2 ">
+                	<div class="m-text1"><b style="font:bold;">DR.LORRIE J.<br>KLEIN<br></b></div>
+                	<div class="m-text2">Featured on the cover of MedEsthetics</div>
+                	<div class="m-text3">
+                		<a href="#"><span>Read article</span></a>
+                	</div>
+                </div>
+        		<div id="col-img-2" class="col-img2" style="position: absolute;"></div>
+        	</div>
+        	<div class="col-3">
+        		<div id="col-text-3" class="col-text3 ">
+        			<div class="l-text1"><b style="font:bold;">VISIT OVR DA<br>SPA NOW!<br></b></div>
+        			<div class="l-text2">Facials are just the start!</div>
+        			<div class="l-text3">
+        				<a href="#"><span>Visit Website</span></a>
+        			</div>
+        		</div>
+        		<div id="col-img-3" class="col-img3"></div>      		
+        	</div>
+        </div>
+        <div class="section_5">
+        	<div class="sec1-sec2">
+        		<div id="sec1" class="sec-image bgt_set"></div>
+        	    <div class="sec2">
+        		    <div class="sec5-text">Beauty Bar MD is OC Dermatology's premier <br>skin care destination for Orange County<br>residents of all skin types</div>
+        		    <a href="#"><span>Learn more</span></a>
+        	    </div>
+        	</div>
+        </div>
+        <div class="section_6">
+        	<div class="form-container">
+        		<div class="form-header"><b style="font: bold;">ASK US ABOUT OUR SERVICES AND SPECIALS</b></div>
+        		<div id="form_c">
+        			<form id="form" name="equiry">
+        				<div id="f_col">
+        					<div id="fld">
+        						<i class="fa fa-user"></i>
+        						<input type="text" name="fn" placeholder="Full Name">
+        					</div>
+        					<div id="fld">
+        						<i class="fa fa-envelope"></i>
+        						<input type="text" name="em" placeholder="Email Address">
+        					</div>
+        					<div id="fld">
+        						<i class="fa fa-phone"></i>
+        						<input type="text" name="ph" placeholder="Phone Number">
+        					</div>
+        				</div>
+        				<div id="f_col">
+        					<i class="fa fa-comment"></i>
+        					<textarea name="message" placeholder="How can we help you?"></textarea>
+        				</div>
+        			</form>
+        		</div>
+        		<!-- <div class="form">
+        			<div class="form-1">
+        				<form>	
+        					<input class="form-input" type="text" name="name" placeholder="Full name" required="">
+        					<input class="form-input" type="text" name="email" placeholder="Email" required="">
+        					<input class="form-input" type="text" name="number" placeholder="Phone Number" required="">
+        				</form>
+        			</div>
+        			<div class="form-2">
+        				<form>
+        					<input class="form-2-input" type="text" name="message" placeholder="How can we help you">
+        				</form>
+        			</div>
+        		</div>
+        		<div class="bottom">
+        			<button class="bottom-submit" type="submit">SUBMIT</button>
+        			<div class="bottom-contact">(949) 363-1788<br>enquiry@ocdermatology.com<br></div>
+        		</div> -->
+        	</div>
+        	<div class="app-container" style="position: absolute;">
+        		<div id="app-container-1" class="app-container-1-img bgt_set">
+        			<div class="app-container-text">
+        				<div class="download-text">DOWNLOAD ONE OF OUR IPHONE <br>APPS WITH BUILT IN APPOINTMENT<br> CALENDARS!</div>
+        				<div class="app-store">
+        					<div class="phone-icon">
+        						<i class="fa-solid fa-mobile-screen-button"></i>
+        					</div>
+        					<div class="app">
+        						<div class="app-txt">Available on the</div>
+        						<div class="app-link"><a href="#">App Store</a></div>
+        					</div>
+        					
+        				</div>
+        			</div>
+        			<div class="app-container-image">
+        				<div class="hand-image1"></div>
+        				<div id="hand-image2" class="hand-img bgt_set"></div>
+        			</div>
+        		</div>
+        		<div class="app-container-2">
+        			<div class="Oc">
+        				<i class="fa-solid fa-circle-check" style="display: inline-block;"></i>
+        				<a href="#"><span style="display: inline-block;">OC Dermatology &gt;&gt;</span></a>
+        			</div>
+        			<div class="Euro">
+        				<i class="fa-solid fa-circle-check" style="display: inline-block;"></i>
+        				<a href="#"><span style="display: inline-block;">Euro Day Spa &gt;&gt;</span></a>
+        			</div>
+        			<div class="Book">
+        				<i class="fa-solid fa-circle-check" style="display: inline-block;"></i>
+        				<a href="#"><span style="display: inline-block;">Book in Appointment4 easy steps</span></a>
+        			</div>
+        		</div>
+        		</div>
+        	</div>
+        </div>
+
+    
+    <div id="awards">
+    	<div class="awards-text">
+    		<div style="font-size: large;">AWARDS &amp; HONORS</div>
+    		<div>Top Rated in Orange County Patient's Guide Platinum Member 2012</div>
+    	</div>
+    	<div class="awards-img">
+    		<div id="awards-image" class="fraxel bgt_set"></div>
+    		<div id="awards-image" class="botox bgt_set"></div>
+    		<div id="awards-image" class="radiesse bgt_set"></div>
+    		<div id="awards-image" class="abd bgt_set"></div>
+    		<div id="awards-image" class="as-ds bgt_set"></div>
+    		<div id="awards-image" class="add bgt_set"></div>
+    		<div id="awards-image" class="p-member bgt_set"></div>
+    	</div>
+    </div>
+    <div id="footer">
+    	<div class="footer1">
+    		<div class="footer-logo">
+    			<div id="f-logo" class="foot_img bgt_set"></div>
+    			<div id="f-icons">
+    				<i class="fa-brands fa-facebook-f" style="
+    padding: 13px 16px;
+"></i>
+    				<i class="fa-brands fa-twitter"></i>
+    				<i class="fa-brands fa-youtube"></i>
+    				<i class="fa-solid fa-wifi" style="
+    padding: 13px 12px;
+    transform: rotate(45deg);
+"></i>
+    			</div>
+    		</div>
+    		<div class="footer-links">
+    			<div class="footer-link-text">
+    				<div class="column_1">
+    					<div class="col1">FOOTER MENU</div>
+    					<a href="#"><span>COSMETIC DERMATOLOGY</span></a>
+    					<a href="#"><span>PHOTO GALLERY</span></a>
+    					<a href="#"><span>NEWS &amp; SPECIALS</span></a>
+    					<a href="#"><span>ABOUT US</span></a>
+    					<a href="#"><span>TESTMONIALS</span></a>
+    					<a href="#"><span>CONTACT US</span></a>
+    				</div>
+    			        <div class="column_2">
+    			        	<div class="col2">COSMETIC DERRMATOLOGY</div>
+    					<a href="#"><span>LASERS &amp; LIGHTS</span></a>
+    					<a href="#"><span>FRAXEL RE: PAIRS®</span></a>
+    					<a href="#"><span style="
+    color: #aaa;
+">FRAXEL RE: STORE®</span></a>
+    					<a href="#"><span>FRAXEL® DUAL LASER PEELL</span></a>
+    					<a href="#"><span>CLEAR + BRILLIANT®</span></a>
+    					<a href="#"><span style="font: bold;">+ MORE</span></a>
+    			        </div>
+    			        <div class="column_3">
+    			        	<div class="col3">REACH US</div>
+    			        	<div class="col_3_text" style="font:bolder; color: #00bcd4;">OC DERMATOLOGY<br>LORRIE KLEIN, M.D.,A.P.C<br></div>
+    			        	<div class="col_3_text">30201 Goldern Lantern<br>Suite B, Laguna Niguel; CA 92677<br></div>
+    			        	<div class="col_3_text">Tel: (949) 363-1788</div>
+    			        </div>
+    			</div>
+    			<div class="footer-link-form">
+    				<div class="column_4">
+    					<form>
+    						<div class="col4">STAY CONNECTED</div>
+    						<input type="text" name="email" placeholder="email">
+    						<button type="submit">SIGN UP</button>
+    					</form>
+    				</div>
+    			</div>
+    		</div>
+    	</div>
+    	<div class="footer2">
+    		<div class="copyright">Copyright @ 2015 Dr. Lorrie Klein Orange County Dermatology. All Rights Reserved</div>
+    		<div class="privacy">
+    			<a href="#">Privacy |</a>
+                        <a href="#">Stemap |</a>
+                        <a href="#">Feedback</a>
+    		</div>
+    	</div>
+    </div>
+
+
+</body></html>
